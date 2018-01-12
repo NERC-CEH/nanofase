f3c5cc12a402875943e77694523a16f6f89d6443
Sam Harrison <samharrison.xg@gmail.com>
2018-01-12 08:25:10 +0000

    Renaming .f08 to .f90 to comply with Intel Fortran.

---
+++ src/Estuary/classEstuaryReach1.f90
@@ -0,0 +1,29 @@
+module classEstuaryReach1
+	use Globals
+	use ResultModule
+	use spcEstuaryReach
+	implicit none
+	
+	type, public, extends(EstuaryReach) :: EstuaryReach1
+		
+	  contains
+	    procedure :: create => createEstuaryReach1
+          procedure :: update => updateEstuaryReach1
+      end type
+	
+  contains
+	
+	function createEstuaryReach1(me) result(r)
+	    class(EstuaryReach1) :: me
+		type(Result) :: r
+		! Create stuff
+	end function
+	
+	function updateEstuaryReach(me, t) result(r)
+	    class(EstuaryReach1) :: me
+		integer :: t
+		type(Result) :: r
+		! Update stuff
+	end function
+	
+end module
