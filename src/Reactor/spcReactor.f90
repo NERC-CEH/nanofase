c5588453e17087ab10a1a40901d6979bf61c50ee
Sam Harrison <samharrison.xg@gmail.com>
2018-01-22 16:21:49 +0000

    Add NP size class as dimension to NP mass matrix in Reactor.

--- src/Reactor/spcReactor.f90
+++ src/Reactor/spcReactor.f90
@@ -8,10 +8,10 @@ module spcReactor
         character(len=100) :: ref
         integer :: x
         integer :: y
-        real(dp), allocatable :: m_np(:,:)
-            !! Array of NP masses, each element representing a different state (1st dimension)
-            !! and form (2nd dimension). States: free, heteroaggreated (per SPM size class).
-            !! Forms: core, shell, coating, corona, adsorbed, dissolved
+        real(dp), allocatable :: m_np(:,:,:)
+            !! Array of NP masses, each element representing a different NP size class (1st dimension),
+            !! state (2nd dimension) and form (3rd dimension). States: free, heteroaggreated
+            !! (per SPM size class). Forms: core, shell, coating, corona, adsorbed, dissolved.
         
         contains
         procedure(createReactor), deferred :: create
