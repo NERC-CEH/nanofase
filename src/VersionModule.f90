b6b2c70ef012d08f47df69a6eaa0012331a23d2b
Sam Harrison <samharrison.xg@gmail.com>
2021-11-26 13:38:18 +0000

    Updates

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,4 +1,4 @@
 module VersionModule
     implicit none
-    character(len=16), parameter :: MODEL_VERSION = "0.0.3-5-gbe163a0"
+    character(len=16), parameter :: MODEL_VERSION = "0.0.3-9-g94378cf"
 end module
