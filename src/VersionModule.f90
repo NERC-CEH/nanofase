44d5acebd3094ebbd199d0312433739df1c10c18
Sam Harrison <samharrison.xg@gmail.com>
2022-09-01 20:15:33 +0100

    Updating test scenario config

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,4 +1,4 @@
 module VersionModule
     implicit none
-    character(len=16), parameter :: MODEL_VERSION = "0.0.4-12-gdeebbab"
+    character(len=17), parameter :: MODEL_VERSION = "0.0.4-14-g9c8b16d"
 end module
