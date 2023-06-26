5297e30996687738c34af8e8b29ce98cb53edf7c
Sam Harrison <samharrison.xg@gmail.com>
2023-06-26 20:55:47 +0100

    Adding BMI library to CMake

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,4 +1,4 @@
 module VersionModule
     implicit none
-    character(len=17), parameter :: MODEL_VERSION = "0.0.4-19-g674d437"
+    character(len=17), parameter :: MODEL_VERSION = "0.0.4-23-ge52e64a"
 end module
