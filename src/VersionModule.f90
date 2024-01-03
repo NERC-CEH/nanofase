d1fb4dc22e304ad9036661f968dac579e7a180e1
Sam Harrison <samharrison.xg@gmail.com>
2024-01-03 13:44:34 +0000

    Consolidating rsp files and updating docs with instructions to escape @ character

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,3 +1,4 @@
 module VersionModule
-implicit none; character(len=17), parameter :: MODEL_VERSION = "0.0.4-24-gdf24b0b"
+    implicit none
+    character(len=17), parameter :: MODEL_VERSION = "0.0.4" 
 end module
