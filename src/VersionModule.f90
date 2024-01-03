84217ebd16d0fb3ed05acd9fb2cd0512205bfd4b
Sam Harrison <samharrison.xg@gmail.com>
2024-01-03 12:24:10 +0000

    Adding Conda recipe and testing setting version number by rsp file

--- src/VersionModule.f90
+++ src/VersionModule.f90
@@ -1,4 +1,3 @@
 module VersionModule
-    implicit none
-    character(len=17), parameter :: MODEL_VERSION = "0.0.4-16-gd0db59a"
+implicit none; character(len=17), parameter :: MODEL_VERSION = "0.0.4-24-gdf24b0b"
 end module
